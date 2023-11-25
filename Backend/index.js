90fb0257b8237029123a897783efeb72f2368067
Sufian Aslam <sufianaslam127@gmail.com>
2023-11-26 03:59:10 +0500

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,7 @@ const app=express();
 app.use(express.json());
 app.use(cors({
     origin: 'https://deploywork-frontend.vercel.app',
+    credentials: true
     // Add any other allowed origins
   }));
   
